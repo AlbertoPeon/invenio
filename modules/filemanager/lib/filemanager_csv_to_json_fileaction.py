811aa78a3065379850a7907e922677ad59d69a8a
Alberto <alberto.rodriguez.peon@cern.ch>
2013-09-06 17:23:21 +0200

    Filemanager: 'Transform CSV to JSON' action adapted to the new Redis Cache
    * Action renamed from 'transform' to 'csv_to_json'
    
    Signed-off-by: Alberto Rodriguez Peon <alberto.rodriguez.peon@cern.ch>

--- modules/filemanager/lib/filemanager_csv_to_json_fileaction.py
+++ modules/filemanager/lib/filemanager_csv_to_json_fileaction.py
@@ -16,51 +16,33 @@
 ## You should have received a copy of the GNU General Public License
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+from invenio.filemanager_helper import FileManagerAction
 import urllib, urllib2, csv, json
 
 """FileManager tranform action Plugin"""
 
-class FileAction(object):
+class FileAction(FileManagerAction):
     """docstring for Visualizer"""
-    name = 'transform'
-    
-    def _csv_to_json(self, params):
-        name = "temp"
-        original_file = params.get('file')
+    name = 'csvtojson'
+    accepted_mimetypes = ['text/plain', 'text/csv']
+    response_mimetype = 'application/json'
+
+    def action(self, *args, **kwargs):
+        """
+        Transforms a CSV file to a JSON file
+        """
+        to_format = kwargs.get('params').get('to')
+        filename = kwargs.get('files')[0]      
+
+        if not filename or not to_format or to_format != 'json': 
+            raise Exception('Wrong params!')
 
         json_file = []
-        csvreader = csv.reader(urllib2.urlopen(urllib.unquote(original_file)))
+        csvreader = csv.reader(urllib2.urlopen(urllib.unquote(filename)))
         header = csvreader.next()
         for line in csvreader:
             dict_line = {}
             for index in range(len(line)):
                 dict_line[header[index]] = line[index]
             json_file.append(dict_line)
-        """
-        with open(create_path_upload(name), 'w') as final_file:
-            final_file.write(json.dumps(json_file))
-        """
-        return name
-
-
-    def _json_to_csv(self, params):
-        name = "temp"
-        
-        return name
-
-    methods = {
-                'csv': _json_to_csv,
-                'json':_csv_to_json
-              }
-  
-    def action(self, params):
-        """
-        Transforms a CSV file to a JSON file or the contrary
-        """
-        to_format = params.get('to')
-        original_file = params.get('file')
-        
-        if (not original_file or not to_format or (to_format != 'csv' 
-                                           and to_format != 'json')):
-            raise Exception('Wrong params!')
-        return self.methods[to_format](self, params)    
+        return json.dumps(json_file)  
